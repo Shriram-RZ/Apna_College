a0d4e57125dc0c0d767159042be541d581745b8a
Shri_Ram <shriram173yt@gmail.com>
2025-02-08 09:53:35 +0530

    Message

---
+++ hi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a , b , sum;
+    printf("Enter A : ");
+    scanf("%d",&a);
+    printf("Enter B : ");
+    scanf("%d" ,&b);
+    sum = a+b;
+    printf("The sum of %d and The sum of %d is %d",a,b,sum);
+    return 0;
+}
