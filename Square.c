32804f39af90c35df99b39e5f79bf42e5d110b4a
Shri_Ram <shriram173yt@gmail.com>
2025-02-08 09:58:10 +0530

    Message'

---
+++ Square.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float side , area;
+    printf("Enter Side of the square : ");
+    scanf("%f",&side);
+    area = side * side;
+    printf("The area of the square is : %f",area);
+}
