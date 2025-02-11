bcb3aad86bed0cc0f81049ab14155e5e33f11445
Shri_Ram <shriram173yt@gmail.com>
2025-02-11 05:34:30 +0530

    Pointer

--- Main.c
+++ Main.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int age = 22;
-    int *ptr = &age;
-    int _age = (int) &ptr;
+    int *ptr;
+    int x = 0;
 
-    char symbol = '*';
-    char *ptr_Symbol = &symbol;
-    char _symbol = *ptr_Symbol;
-    printf("%p \n",&age);
-    printf("%p \n",ptr); //hexadecimal
-    printf("%p \n",&ptr);
-    printf("%d \n",*ptr);
+    ptr = &x;
+    *ptr = 0;
 
-    printf("%c \n",_symbol);
+    printf(" x = %d\n",x);
+    printf(" *ptr = %d\n",*ptr);
 
+    *ptr += 5;
 
-    printf("%d \n",_age);
-    printf("%p \n",&_age);
-    printf("%u \n",ptr); //Number
-    printf("%u \n",&age);
-    printf("%p",symbol);
+    printf(" x = %d\n",x);
+    printf(" *ptr = %d\n",*ptr);
+
+    (*ptr)++;
+
+    printf(" x = %d\n",x);
+    printf(" *ptr = %d\n",*ptr);
     
 }
