ad9721e5ca7f52a314ff78234c29cf38c3b5f9be
Shri_Ram <shriram173yt@gmail.com>
2025-02-12 09:26:31 +0530

    Message

--- Main.c
+++ Main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
-void doWork(int a , int b , int *sum , int *prod , int *avg);
+void printNumbers(int arr[],int n,int size);
 
 int main(){
-    int a , b , sum , prod , avg;
-    a = 10;
-    b = 20;
-
-    doWork(a,b,&sum,&prod,&avg);
-    printf("The Sum is : %d \n The Product is : %d \n The Average is : %d",sum,prod,avg);
+    int number , size;
+    int arr[100] = {4, 1, 2, 5, 3, 4, 2, 5, 1, 3,
+        2, 4, 5, 1, 3, 2, 5, 4, 1, 3,
+        5, 2, 4, 3, 1, 5, 2, 4, 1, 3,
+        2, 5, 4, 3, 1, 5, 4, 2, 3, 1,
+        4, 5, 3, 2, 1, 4, 5, 3, 2, 1,
+        5, 4, 3, 2, 1, 5, 3, 4, 2, 1,
+        4, 3, 5, 2, 1, 4, 5, 3, 1, 2,
+        5, 4, 3, 1, 2, 5, 4, 3, 2, 1,
+        4, 5, 3, 2, 1, 4, 5, 2, 3, 1,
+        5, 4, 3, 1, 2, 5, 4, 3, 2, 1};
+    number = 4;
+    size = sizeof(arr) / sizeof(arr[0]);
+    printNumbers(arr,number,size);
 }
 
+void printNumbers(int arr[],int n,int size){
+    for(int i = 0 ; i < size ; i++){
+        if(arr[i] == n){
+        printf("%d \t",i);
 
-void doWork(int a , int b , int *sum , int *prod , int *avg){
-    *sum = a + b;
-    *prod = a * b;
-    *avg = (a+b)/2;
+
+        }
+    }
 }
